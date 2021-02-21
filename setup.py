53c36600a294810b4789cc179f4e69c605a96c8a
suproteemsarkar <suproteemsarkar@users.noreply.github.com>
2021-02-21 17:57:24 -0500

    BUG: Find all packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """Set up the package."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -9,7 +9,7 @@ setup(
     name="nostocalean",
     version="0.0.1",
     author="Suproteem Sarkar",
-    packages=["nostocalean", "nostocalean.ols"],
+    packages=find_packages(),
     python_requires=">=3.5",
     description="Custom utility functions.",
     long_description=long_description,
