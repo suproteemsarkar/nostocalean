e31da5be531c49604f52cd315731ebbd93fa4144
suproteemsarkar <suproteemsarkar@users.noreply.github.com>
2021-02-23 15:45:44 -0500

    API: Move environment-changing imports into nt

--- nostocalean/__init__.py
+++ nostocalean/__init__.py
@@ -1,5 +1,3 @@
 """Top level nostocalean functions."""
 
 from .functions import clean_name, lmap, skipna, suppress
-from .plotting import palette
-from . import flavor
---
+++ nostocalean/nt.py
@@ -0,0 +1,5 @@
+"""Standard nostocalean functions and plot themes."""
+
+from .functions import clean_name, lmap, skipna, suppress
+from .plotting import palette
+from .flavor import mem
