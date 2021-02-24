1e623a08955e1ee9be4f113e302f18af880ffc07
suproteemsarkar <suproteemsarkar@users.noreply.github.com>
2021-02-24 16:53:59 -0500

    API: Move fixest wrapper to est.py, add glm

--- README.rst
+++ README.rst
@@ -7,16 +7,29 @@ Custom utility functions.
 This package contains a collection of utility functions for empirical work in Python.
 It's in progress and the API may change often.
 
-Nostocalean includes a wrapper for ``fixest::feols``
+Nostocalean includes a wrapper for ``fixest``
 
-Example: Given a dataframe ``df`` with columns ``x, y, group, time`` 
-run a regression of ``y`` on ``x`` with group and time fixed effects, and double cluster:
+Example: Given a dataframe ``df`` with columns ``x, y, group_a, group_b`` 
+run a Poisson regression of ``y`` on ``x`` with group fixed effects, and double cluster:
 
 .. code-block:: python
 
-    from nostocalean import ols
+    from nostocalean import est
     
-    summary = ols.reg("y ~ x | group + time", data=df, se="twoway")
+    summary = est.feglm("y ~ x | group + time", data=df, se="twoway").summary()
+    print(summary)
+
+There are additional utility methods for ols: ``est.reg``, which returns a string regression summary,
+and ``est.treg``, which returns a coefficient table.
+
+Example: Given a dataframe ``df`` with columns ``x, y, group_a, group_b`` 
+run a regression of ``y`` on ``x`` with group fixed effects, and double cluster:
+
+.. code-block:: python
+
+    from nostocalean import est
+    
+    summary = est.reg("y ~ x | group + time", data=df, se="twoway")
     print(summary)
 
 Nostocalean also includes several utility functions, including CES and HARA
---
+++ nostocalean/est.py
@@ -0,0 +1,85 @@
+"""Methods for calling fixest using rpy2."""
+
+from typing import Optional, TypeVar
+
+from rpy2.robjects import Formula, pandas2ri
+from rpy2.robjects.packages import importr
+import pandas as pd
+from .functions import clean_name, suppress
+
+FixestResult = TypeVar("FixestResult")
+
+base = importr("base")
+fixest = importr("fixest")
+pandas2ri.activate()
+
+
+class RegressionResult:
+    """Accessors for a fixest result."""
+
+    def __init__(self, result: FixestResult, se: str):
+        self.result = result
+        self.rx = self.result.rx
+        self.se = se
+
+    def summary(self, **kwargs) -> str:
+        """Return a string summary of a feols result."""
+        if "se" not in kwargs:
+            kwargs["se"] = self.se
+
+        with suppress():
+            return str(base.summary(self.result, **kwargs))  # pylint: disable=no-member
+
+    def get_table(self) -> pd.DataFrame:
+        """Return the coefficient table from a feols regression result."""
+        return (
+            self.result.rx["coeftable"][0]
+            .rename(columns=clean_name)
+            .rename(columns={"pr_t": "p_value"})
+        )
+
+
+def feols(
+    fml: str,
+    data: pd.DataFrame,
+    se: Optional[str] = None,
+    **kwargs,
+) -> RegressionResult:
+    """Wrapper for calling fixest::feols in R."""
+
+    if se == None:
+        se = "cluster" if "cluster" in kwargs else "hetero"
+
+    # fmt: off
+    result  = fixest.feols(Formula(fml), data=data, se=se, **kwargs) # pylint: disable=no-member
+    # fmt: on
+
+    return RegressionResult(result, se=se)
+
+
+def feglm(
+    fml: str,
+    data: pd.DataFrame,
+    se: Optional[str] = None,
+    **kwargs,
+) -> RegressionResult:
+    """Wrapper for calling fixest::feglm in R."""
+
+    if se is None:
+        se = "cluster" if "cluster" in kwargs else "hetero"
+
+    # fmt: off
+    result  = fixest.feglm(Formula(fml), data=data, se=se, **kwargs) # pylint: disable=no-member
+    # fmt: on
+
+    return RegressionResult(result, se=se)
+
+
+def reg(**kwargs) -> str:
+    """Run a feols regression and return the summary."""
+    return feols(**kwargs).summary()
+
+
+def treg(**kwargs) -> pd.DataFrame:
+    """Run a feols regression and return the coefficient table."""
+    return feols(**kwargs).get_table()
