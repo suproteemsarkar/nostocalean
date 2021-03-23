d3886ab71312cc2816425033c4fc9dbc68bfb99f
suproteemsarkar <suproteemsarkar@users.noreply.github.com>
2021-03-22 21:33:19 -0400

    ENH: Warn if no did installed

--- nostocalean/est/__init__.py
+++ nostocalean/est/__init__.py
@@ -1,6 +1,11 @@
+import warnings
 from rpy2.robjects import pandas2ri
 
 from nostocalean.est.fixest import feols, feglm, reg, treg, preg
-from nostocalean.est.did import att_gt, did, es, pdid, pes
+
+try:
+    from nostocalean.est.did import att_gt, did, es, pdid, pes
+except:
+    warnings.warn("Failed to load did methods")
 
 pandas2ri.activate()
