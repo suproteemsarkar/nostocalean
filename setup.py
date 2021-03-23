282d25ac6cc9f6e5fb8c09691284afb5555ae15a
suproteemsarkar <suproteemsarkar@users.noreply.github.com>
2021-03-22 21:33:37 -0400

    REL: Bump version to 0.0.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="nostocalean",
-    version="0.0.11",
+    version="0.0.12",
     author="Suproteem Sarkar",
     packages=find_packages(),
     python_requires=">=3.5",
