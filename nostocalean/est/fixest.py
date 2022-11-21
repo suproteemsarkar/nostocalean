8463c7118bda7c13a24e18cdc95cd1f0a2005b79
Suproteem Sarkar <suproteemsarkar@users.noreply.github.com>
2022-11-20 20:01:26 -0500

    API: Add compatibility with new fixest interface

--- nostocalean/est/fixest.py
+++ nostocalean/est/fixest.py
@@ -6,7 +6,7 @@ from typing import Optional
 from rpy2 import robjects
 from rpy2.robjects import packages
 import pandas as pd
-from nostocalean.functions import clean_name, suppress
+from nostocalean.functions import suppress
 
 RegressionResult = robjects.vectors.ListVector
 
@@ -17,80 +17,96 @@ fixest = packages.importr("fixest")
 class FixestResult:
     """Accessors for a fixest result."""
 
-    def __init__(self, result: robjects.vectors.ListVector, se: str):
+    def __init__(self, result: robjects.vectors.ListVector, **kwargs):
         self.result = result
         self.rx = self.result.rx
-        self.se = se
+        if "vcov" in kwargs:
+            self.mode = "vcov"
+            self.vcov = kwargs["vcov"]
+        else:
+            self.mode = "cluster"
+            self.cluster = kwargs["cluster"]
 
     def summary(self, **kwargs) -> str:
         """Return a string summary of a feols result."""
-        if "se" not in kwargs:
-            kwargs["se"] = self.se
+        if "vcov" not in kwargs and "cluster" not in kwargs:
+            if self.mode == "vcov":
+                kwargs["vcov"] = self.vcov
+            else:
+                kwargs["cluster"] = self.cluster
 
         with suppress():
             return str(base.summary(self.result, **kwargs))  # pylint: disable=no-member
 
-    def get_table(self) -> pd.DataFrame:
+    def get_table(self, **kwargs) -> pd.DataFrame:
         """Return the coefficient table from a feols regression result."""
-        return (
-            self.result.rx["coeftable"][0]
-            .rename(columns=clean_name)
-            .rename(columns={"pr_t": "p_value"})
+        coeftable = pd.DataFrame(
+            self.rx["coeftable"][0], columns=["coef", "se", "t", "p"]
         )
+        summary = self.summary()
+        coeftable.index = map(
+            lambda s: s.split(f" ")[0],
+            summary[summary.find("Standard-errors") : summary.find("\n---")].split(
+                "\n"
+            )[2:],
+        )  # Get coefficient names from fixest summary
+        return coeftable
 
 
 def feols(
     fml: str,
     data: pd.DataFrame,
-    se: Optional[str] = None,
     **kwargs,
 ) -> FixestResult:
     """Wrapper for calling fixest::feols in R."""
 
-    if se is None:
-        se = "cluster" if "cluster" in kwargs else "hetero"
+    if "vcov" not in kwargs and "cluster" not in kwargs:
+        kwargs["vcov"] = "hetero"
 
     columns = set(re.findall(r"[\w']+", fml))
     columns = [column for column in columns if column != "1"]
 
-    if "cluster" in kwargs:
-        columns = columns + list(set(re.findall(r"[\w']+", kwargs["cluster"])))
+    for key in ["cluster", "panel_id"]:
+        if key in kwargs:
+            columns = list(set(columns + re.findall(r"[\w']+", kwargs[key])))
+            if kwargs[key][0] == "~":
+                kwargs[key] = robjects.Formula(kwargs[key])
 
     result = fixest.feols(  # pylint: disable=no-member
         robjects.Formula(fml),
         data=data[columns].dropna(subset=columns),
-        se=se,
         **kwargs,
     )
 
-    return FixestResult(result, se=se)
+    return FixestResult(result, **kwargs)
 
 
 def feglm(
     fml: str,
     data: pd.DataFrame,
-    se: Optional[str] = None,
     **kwargs,
 ) -> FixestResult:
     """Wrapper for calling fixest::feglm in R."""
 
-    if se is None:
-        se = "cluster" if "cluster" in kwargs else "hetero"
+    if "vcov" not in kwargs and "cluster" not in kwargs:
+        kwargs["vcov"] = "hetero"
 
     columns = set(re.findall(r"[\w']+", fml))
     columns = [column for column in columns if column != "1"]
 
-    if "cluster" in kwargs:
-        columns = columns + list(set(re.findall(r"[\w']+", kwargs["cluster"])))
+    for key in ["cluster", "panel_id"]:
+        if key in kwargs:
+            columns = list(set(columns + re.findall(r"[\w']+", kwargs[key])))
+            if kwargs[key][0] == "~":
+                kwargs[key] = robjects.Formula(kwargs[key])
 
     result = fixest.feglm(  # pylint: disable=no-member
         robjects.Formula(fml),
         data=data[columns].dropna(subset=columns),
-        se=se,
         **kwargs,
     )
 
-    return FixestResult(result, se=se)
+    return FixestResult(result, **kwargs)
 
 
 def reg(*args, **kwargs) -> str:
